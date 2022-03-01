df9bbd724a3c5aad4abc8d231098ac5a9f28a6d7
Alessandra Costa <alessandracosta8@gmail.com>
2022-03-01 13:15:30 +0000

    Add Open Trivia Questions API to load questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,32 @@ const game = document.getElementById('game');
 -> Load questions from JSON in correct positions
 -> Hide the loader and begin the game*/
 let questions = [];
-fetch('/questions.json')
+fetch(
+    'https://opentdb.com/api.php?amount=50&category=11&difficulty=easy&type=multiple'
+)
     .then((res) => {
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions;
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
         startGame();
     })
     .catch((err) => {
