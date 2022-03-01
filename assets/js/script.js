e84cd768f1e3da8033be7b164223999c6e702013
Alessandra Costa <alessandracosta8@gmail.com>
2022-03-01 13:10:44 +0000

    Add correction on page links 19

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ const game = document.getElementById('game');
 -> Load questions from JSON in correct positions
 -> Hide the loader and begin the game*/
 let questions = [];
-fetch('questions.json')
+fetch('/questions.json')
     .then((res) => {
         return res.json();
     })
